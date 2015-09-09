1f74c44b6f649809bde2a5891c77f31428ed2bca
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-09-09 16:10:17 +0800

    ft(routing): updated routing to work on ionic app as well

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: '/auth'
   root 'static_pages#index'
 
-  resources :forms
+  post 'users/:user_id/forms', to: 'forms#create'
+  get 'users/:user_id/forms', to: 'forms#index'
+  resources :forms, only: [:destroy, :show, :update]
 
   post '/forms/:form_id/submissions', to: 'submissions#create'
   get '/forms/:form_id/submissions', to: 'submissions#index'
-  resources :submissions
+  resources :submissions, only: [:destroy, :show, :update]
 
   post '/forms/:form_id/contents', to: 'contents#create'
   get '/forms/:form_id/contents', to: 'contents#index'
-  resources :contents
+  resources :contents, only: [:destroy, :show, :update]
 
   post '/contents/:content_id/answers', to: 'answers#create'
 end
