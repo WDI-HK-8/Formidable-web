51f81daefc6881b81a1b6e206cd999f77146882c
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-09-09 10:51:28 +0800

    ft(form_controller): updated delete and update methods

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -16,8 +16,11 @@ class FormsController < ApplicationController
     if @form.nil?
       render json: {message: "Cannot find form"}, status: :not_found
     else
-      @form.update(form_params)
-      render json: {updated: true}, status: :success
+      if @form.update(form_params)
+        render json: {updated: true}, status: :success
+      else
+        render json: {updated: false}, status: :bad_request
+      end
     end
   end
 
@@ -26,8 +29,12 @@ class FormsController < ApplicationController
     if @form.nil?
       render json: {message: "Cannot find form"}, status: :not_found
     else
-      @form.destroy
-      render json: {deleted: true}, status: :success
+      else
+      if @form.destroy
+        render json: {deleted: true}, status: :success
+      else
+        render json: {deleted: false}, status: :bad_request
+      end
     end
   end
 
